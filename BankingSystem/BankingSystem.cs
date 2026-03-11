ee122b677882fa9e10f9939ce2bcbcabf057096d
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-11 19:36:12 +0000

    feat: implemented a search bank account method

--- BankingSystem/BankingSystem.cs
+++ BankingSystem/BankingSystem.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System; 
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +6,23 @@ namespace BankingSystem
 {
     internal class BankingSystem
     {
+        public List<BankAccount> Accounts { get; private set; }
+        public BankingSystem()
+        {
+            this.Accounts = new List<BankAccount>();
+        }
+        // Search needs to check if the account num is found in our list of bank accounts, if so we return that account so we can perform more actions for the registered user, otherwise return null
+        public BankAccount Search(string accounNumbert)
+        {
+            foreach (BankAccount account in Accounts)
+            {
+                if (account.AccountNumber == accounNumbert)
+                {
+                    return account;
+                }
+                
+            }
+            return null;
+        }
     }
 }
