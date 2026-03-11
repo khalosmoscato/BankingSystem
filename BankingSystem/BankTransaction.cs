3d203169ed2a618412e58e6eadcfa22b216cd6d4
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-11 15:36:38 +0000

    feat: initialised the BankTransaction logic

--- BankingSystem/BankTransaction.cs
+++ BankingSystem/BankTransaction.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace BankingSystem
 {
-    internal class BankTransaction
-    {
-    }
+    // changed from `class` to `record` to make this data immutable
+    public record BankTransaction(
+        int TransactionId,
+        string Type,
+        decimal Amount,
+        DateTime Timestamp,
+        string Category
+        );
 }
