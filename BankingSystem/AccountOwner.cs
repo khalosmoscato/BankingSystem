5833f33d2c1f84709f4527bfb724f90dc0baa6ab
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-11 15:21:51 +0000

    feat: created the folder structure and initialised the AccountOwner class

---
+++ BankingSystem/AccountOwner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingSystem
+{
+    internal class AccountOwner
+    {
+        public int CustomerId { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public AccountOwner(int customerId, string firstName, string lastName)
+        {
+            this.CustomerId = customerId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
